cb7af8107877f9ee74c1a695e09f53507d1c0e19
Thomas Nicholas <tom@d151-91.genetics.utah.edu>
2018-05-18 11:26:11 -0600

    added status output statements

--- prepare_ensembl.sh
+++ prepare_ensembl.sh
@@ -6,8 +6,10 @@ set -euo pipefail
 
 #Get the human alternate names from gene_info.gz
 #Skip genes without alternate names (column 2 is '-')
+echo "Extracting human only gene names with alternates"
 gzcat gene_info.gz | awk '$1==9606{print $3"\t"$5}' | awk '$2!="-"' | sort > entrez_gene_synonyms.txt
 
 #Create the Ensembl gene coords list with all alternate names included
+echo "Creating sorted.ensembl.gene.coords file"
 python ensembl_gene_coords.py | gsort /dev/stdin ~/work/genome_ref/genome.nochr.file > sorted.ensembl.gene.coords
 
