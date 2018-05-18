1a8540971fabb50e7319221d239e829639f1ae15
Thomas Nicholas <tom@d151-91.genetics.utah.edu>
2018-05-18 11:18:18 -0600

    adding initial scripts for getting ensembl files and prepping them to make ensembl gene coord file

---
+++ prepare_ensembl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#To be used in connection with get_ensembl.sh
+
+set -euo pipefail
+
+#Get the human alternate names from gene_info.gz
+#Skip genes without alternate names (column 2 is '-')
+gzcat gene_info.gz | awk '$1==9606{print $3"\t"$5}' | awk '$2!="-"' | sort > entrez_gene_synonyms.txt
+
+#Create the Ensembl gene coords list with all alternate names included
+python ensembl_gene_coords.py | gsort /dev/stdin ~/work/genome_ref/genome.nochr.file > sorted.ensembl.gene.coords
+
