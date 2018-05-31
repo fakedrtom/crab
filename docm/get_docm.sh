4cddc0016428513e1a4ca78931352b945158de93
Thomas Nicholas <tom@thomass-mbp.uconnect.utah.edu>
2018-05-31 15:52:04 -0600

    added zipping and indexing to bed file

--- docm/get_docm.sh
+++ docm/get_docm.sh
@@ -5,3 +5,8 @@ curl -o docm.tsv -L http://www.docm.info/api/v1/variants.tsv?versions=3.2
 echo "Preparing BED file"
 python make_docm_bed.py > docm.bed
 
+echo "Sorted and gzipping the BED files"
+gsort docm.bed /Users/tom/work/genome_ref/genome.nochr.file | bgzip -c > docm.bed.gz
+
+echo "Create an index, using tabix, of the zipped bed files"
+tabix -p bed docm.bed.gz
