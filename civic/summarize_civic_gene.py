6358c259183259efec22e165caf142da6a5e2d1a
Thomas Nicholas <tom@d150-203.genetics.utah.edu>
2019-01-25 17:17:29 -0700

    added cancer name abbreviations and sorting to bed outputs

--- civic/summarize_civic_gene.py
+++ civic/summarize_civic_gene.py
@@ -5,7 +5,8 @@ from scipy import stats
 
 gene_file = open("tmp.civic_genes.bed", "r")
 gene_out = open("civic_genes_summaries.bed", "w")
-print("#chromosome\tstart\tstop\tgene\tn_variant\tvariant_IDs\tn_evidence\tevidence_IDs\tmax_evidence\tmax_rating\tevidence_type\tevidence_direction\tclinical_significance\tvariant_origin\tdisease\tmax_score\tcivic_per", file=gene_out)
+abbrs = open("/Users/tom/src/cancer_annotations/cancer_names_abbreviations.txt", "r")
+print("#chromosome\tstart\tstop\tgene\tn_variant\tvariant_IDs\tn_evidence\tevidence_IDs\tmax_evidence\tmax_rating\tevidence_type\tevidence_direction\tclinical_significance\tvariant_origin\tdisease\tabbreviations\tmax_score\tcivic_per", file=gene_out)
 
 coords = {}
 variants = defaultdict(list)
@@ -26,14 +27,14 @@ for i in gene_file:
     end = line[2]
     gene = line[3]
     coords[gene]=[chrom, start, end]
-    var_id = line[9] if line[9] != "" else 'None'
+    var_id = line[9] if line[9] != "" else 'none'
     variants[gene].append(var_id)
-    var_score = line[11] if line[11] != "" else 'None'
-    if var_score != 'None':
+    var_score = line[11] if line[11] != "" else 'none'
+    if var_score != 'none' and var_score != 'none':
         id_score[var_id] = var_score
-    evi_type = line[12] if line[12] != "" else 'None'
+    evi_type = line[12] if line[12] != "" else 'none'
     types[gene].append(evi_type.rstrip())
-    evi_level = line[13] if line[13] != "" else 'None' 
+    evi_level = line[13] if line[13] != "" else 'none' 
     if evi_level == 'A':
         evi_level = 5
     elif evi_level == 'B':
@@ -44,27 +45,32 @@ for i in gene_file:
         evi_level = 2
     elif evi_level == 'E':
         evi_level = 1
-    elif evi_level == 'None':
+    elif evi_level == 'none':
         evi_level = 0
     levels[gene].append(evi_level)
-    evi_dir = line[14] if line[14] != "" else 'None'
+    evi_dir = line[14] if line[14] != "" else 'none'
     directions[gene].append(evi_dir.rstrip())
-    clin_sig = line[15] if line[15] != "" else 'None'
+    clin_sig = line[15] if line[15] != "" else 'none'
     clins[gene].append(clin_sig.rstrip())
     rating = line[16] if line[16] != "" else 0
-    if rating == 'None':
+    if rating == 'none':
         rating = 0
     ratings[gene].append(rating)
-    evi_id = line[17] if line[17] != "" else 'None'
+    evi_id = line[17] if line[17] != "" else 'none'
     evidences[gene].append(evi_id.rstrip())
-    origin = line[18] if line[18] != "" else 'None'
+    origin = line[18] if line[18] != "" else 'none'
     origins[gene].append(origin.rstrip())
-    disease = line[19] if line[19] != "" else 'None'
+    disease = line[19] if line[19] != "" else 'none'
     diseases[gene].append(disease.rstrip())
 
 score_list = []
 for id in id_score:
     score_list.append(float(id_score[id]))
+
+cancers = {}
+for line in abbrs:
+    names = line.rstrip("\n").split("\t")
+    cancers[names[0]] = names[1]
 
 for gene in coords:
     max_level = max(levels[gene])
@@ -79,7 +85,7 @@ for gene in coords:
     if max_level == 1:
         max_level = 'E'
     if max_level == 0:
-        max_level = 'None'
+        max_level = 'none'
     max_rating = max(ratings[gene])
     num_var = len(set(variants[gene]))
     var_ids = set(variants[gene])
@@ -89,25 +95,34 @@ for gene in coords:
             scores.append(id_score[id])
     max_score = max(scores) if len(scores) > 0 else 0
     evi_ids = set(evidences[gene])
-    if len(evi_ids) > 1 and 'None' in evi_ids:
-        evi_ids.remove('None')
+    if len(evi_ids) > 1 and 'none' in evi_ids:
+        evi_ids.remove('none')
     num_evi = len(evi_ids)
     evi_types = set(types[gene])
-    if len(evi_types) > 1 and 'None' in evi_types:
-        evi_types.remove('None')
+    if len(evi_types) > 1 and 'none' in evi_types:
+        evi_types.remove('none')
     evi_dirs = set(directions[gene])
-    if len(evi_dirs) > 1 and 'None' in evi_dirs:
-        evi_dirs.remove('None')
+    if len(evi_dirs) > 1 and 'none' in evi_dirs:
+        evi_dirs.remove('none')
     clin_sigs = set(clins[gene])
-    if len(clin_sigs) > 1 and 'None' in clin_sigs:
-        clin_sigs.remove('None')
+    if len(clin_sigs) > 1 and 'none' in clin_sigs:
+        clin_sigs.remove('none')
     origs = set(origins[gene])
-    if len(origs) > 1 and 'None' in origs:
-        origs.remove('None')
+    if len(origs) > 1 and 'none' in origs:
+        origs.remove('none')
     subtypes = set(diseases[gene])
-    if len(subtypes) > 1 and 'None' in subtypes:
-        subtypes.remove('None')
-    if max_level == 'None':
+    if len(subtypes) > 1 and 'none' in subtypes:
+        subtypes.remove('none')
+    abbr = []
+    for s in subtypes:
+        if s == 'none':
+            abbr.append(s)
+        else:
+            if s.lower() in cancers:
+                abbr.append(cancers[s.lower()])
+            if s.lower() not in cancers:
+                print(s.lower()+' not recognized')
+    if max_level == 'none':
         num_evi = 0
     score_per = round(stats.percentileofscore(score_list, float(max_score)),3)
-    print("\t".join(coords[gene]) + "\t" + gene + "\t" + str(num_var) + "\t" + ",".join(var_ids) + "\t" + str(num_evi) + "\t" + ",".join(evi_ids) + "\t" + max_level + "\t" + str(max_rating) + "\t" + ",".join(evi_types) + "\t" + ",".join(evi_dirs) + "\t" + ",".join(clin_sigs) + "\t" + ",".join(origs) + "\t" + ",".join(subtypes) + "\t" + str(max_score) + "\t" + str(score_per), file=gene_out)    
+    print("\t".join(coords[gene]) + "\t" + gene + "\t" + str(num_var) + "\t" + ",".join(sorted(var_ids)) + "\t" + str(num_evi) + "\t" + ",".join(sorted(evi_ids)) + "\t" + max_level + "\t" + str(max_rating) + "\t" + ",".join(sorted(evi_types)) + "\t" + ",".join(sorted(evi_dirs)) + "\t" + ",".join(sorted(clin_sigs)) + "\t" + ",".join(sorted(origs)) + "\t" + ",".join(sorted(subtypes)) + "\t" + ",".join(sorted(set(abbr))) + "\t" + str(max_score) + "\t" + str(score_per), file=gene_out)
--- civic/summarize_civic_variant.py
+++ civic/summarize_civic_variant.py
@@ -5,7 +5,8 @@ from scipy import stats
 
 var_file = open("tmp.civic_variants.bed", "r")
 var_out = open("civic_variants_summaries.bed", "w")
-print("#chromosome\tstart\tstop\tref\talt\tvariant_ID\tvariant_type\tcivic_score\tcivic_per\tn_evidence\tevi_IDs\tevi_types\tmax_level\tevi_directions\tclin_sigs\tmax_rating\torigins\tdiseases", file=var_out)
+abbrs = open("/Users/tom/src/cancer_annotations/cancer_names_abbreviations.txt", "r")
+print("#chromosome\tstart\tstop\tref\talt\tvariant_ID\tvariant_type\tcivic_score\tcivic_per\tn_evidence\tevi_IDs\tevi_types\tmax_level\tevi_directions\tclin_sigs\tmax_rating\torigins\tdiseases\tabbreviations", file=var_out)
 
 coords = {}
 evidences = defaultdict(list)
@@ -26,14 +27,14 @@ for i in var_file:
     ref = line[3]
     alt = line[4]
     varid = line[5]
-    vartype = line[6] if line[6] != "" else 'None'
+    vartype = line[6] if line[6] != "" else 'none'
     score = line[7]
-    if score != 'None':
+    if score != 'none':
         id_score[varid] = score
     coords[varid]=[chrom, start, end, ref, alt, varid, vartype, score]
-    evi_type = line[8] if line[8] != "" else 'None'
+    evi_type = line[8] if line[8] != "" else 'none'
     types[varid].append(evi_type.rstrip())
-    evi_level = line[9] if line[9] != "" else 'None' 
+    evi_level = line[9] if line[9] != "" else 'none' 
     if evi_level is 'A':
         evi_level = 5
     elif evi_level is 'B':
@@ -44,26 +45,31 @@ for i in var_file:
         evi_level = 2
     elif evi_level is 'E':
         evi_level = 1
-    elif evi_level is 'None':
+    elif evi_level is 'none':
         evi_level = 0
     levels[varid].append(evi_level)
-    evi_dir = line[10] if line[10] != "" else 'None'
+    evi_dir = line[10] if line[10] != "" else 'none'
     directions[varid].append(evi_dir.rstrip())
-    clin_sig = line[11] if line[11] != "" else 'None'
+    clin_sig = line[11] if line[11] != "" else 'none'
     clins[varid].append(clin_sig.rstrip())
     rating = line[12] if line[12] != "" else 0
     ratings[varid].append(rating)
-    evi_id = line[13] if line[13] != "" else 'None'
+    evi_id = line[13] if line[13] != "" else 'none'
     evidences[varid].append(evi_id.rstrip())
-    origin = line[14] if line[14] != "" else 'None'
+    origin = line[14] if line[14] != "" else 'none'
     origins[varid].append(origin.rstrip())
-    disease = line[15] if line[15] != "" else 'None'
+    disease = line[15] if line[15] != "" else 'none'
     diseases[varid].append(disease.rstrip())
 
 scores = []
 for id in id_score:
     scores.append(float(id_score[id]))
 
+cancers = {}
+for line in abbrs:
+    names = line.rstrip("\n").split("\t")
+    cancers[names[0]] = names[1]
+    
 for id in coords:
     max_level = max(levels[id])
     if max_level is 5:
@@ -77,28 +83,37 @@ for id in coords:
     if max_level is 1:
         max_level = 'E'
     if max_level is 0:
-        max_level = 'None'
+        max_level = 'none'
     max_rating = max(ratings[id])
     evi_ids = set(evidences[id])
-    if len(evi_ids) > 1 and 'None' in evi_ids:
-        evi_ids.remove('None')
+    if len(evi_ids) > 1 and 'none' in evi_ids:
+        evi_ids.remove('none')
     num_evi = len(evi_ids)
     evi_types = set(types[id])
-    if len(evi_types) > 1 and 'None' in evi_types:
-        evi_types.remove('None')
+    if len(evi_types) > 1 and 'none' in evi_types:
+        evi_types.remove('none')
     evi_dirs = set(directions[id])
-    if len(evi_dirs) > 1 and 'None' in evi_dirs:
-        evi_dirs.remove('None')
+    if len(evi_dirs) > 1 and 'none' in evi_dirs:
+        evi_dirs.remove('none')
     clin_sigs = set(clins[id])
-    if len(clin_sigs) > 1 and 'None' in clin_sigs:
-        clin_sigs.remove('None')
+    if len(clin_sigs) > 1 and 'none' in clin_sigs:
+        clin_sigs.remove('none')
     origs = set(origins[id])
-    if len(origs) > 1 and 'None' in origs:
-        origs.remove('None')
+    if len(origs) > 1 and 'none' in origs:
+        origs.remove('none')
     subtypes = set(diseases[id])
-    if len(subtypes) > 1 and 'None' in subtypes:
-        subtypes.remove('None')
-    if max_level == 'None':
+    if len(subtypes) > 1 and 'none' in subtypes:
+        subtypes.remove('none')
+    abbr = []
+    for s in subtypes:
+        if s == 'none':
+            abbr.append(s)
+        else:
+            if s.lower() in cancers:
+                abbr.append(cancers[s.lower()])
+            if s.lower() not in cancers:
+                print(s.lower()+' not recognized')
+    if max_level == 'none':
         num_evi = 0
     score_per = round(stats.percentileofscore(scores, float(coords[id][7])),3)
-    print("\t".join(coords[id]) + "\t" + str(score_per) + "\t" + str(num_evi) + "\t" + ",".join(evi_ids) + "\t" + ",".join(evi_types) + "\t" + max_level + "\t" + ",".join(evi_dirs) + "\t" + ','.join(clin_sigs) + "\t" + str(max_rating) + "\t" + ",".join(origs) + "\t" + ",".join(subtypes), file=var_out)
+    print("\t".join(coords[id]) + "\t" + str(score_per) + "\t" + str(num_evi) + "\t" + ",".join(sorted(evi_ids)) + "\t" + ",".join(sorted(evi_types)) + "\t" + max_level + "\t" + ",".join(sorted(evi_dirs)) + "\t" + ','.join(sorted(clin_sigs)) + "\t" + str(max_rating) + "\t" + ",".join(sorted(origs)) + "\t" + ",".join(sorted(subtypes)) + "\t" + ",".join(sorted(set(abbr))), file=var_out)
