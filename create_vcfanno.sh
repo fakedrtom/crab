3abb927d74db58e051c103a539dcdf0e76639db6
Thomas Nicholas <tom@d150-203.genetics.utah.edu>
2019-01-28 16:15:35 -0700

    fixed column numbers

--- create_vcfanno.sh
+++ create_vcfanno.sh
@@ -4,13 +4,13 @@ DIR=`pwd`
 #Adding CIViC
 echo '[[annotation]]' > vcfanno.conf
 echo 'file="'$DIR'/civic/civic_genes_summaries.bed.gz"' >> vcfanno.conf
-echo 'columns=[4,5,6,7,8,9,10,11,12,13,14,15,16,17]' >> vcfanno.conf
+echo 'columns=[4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]' >> vcfanno.conf
 echo 'names=["civic_gene","civic_gene_n_vars","civic_gene_var_ids","civic_gene_n_evis","civic_gene_evi_ids","civic_gene_max_level","civic_gene_max_rating","civic_gene_evi_types","civic_gene_evi_directions","civic_gene_clin_sigs","civic_gene_origins","civic_gene_diseases","civic_gene_abbreviations","civic_gene_max_score","civic_gene_percentile"]' >> vcfanno.conf
 echo 'ops=["self","max","self","max","self","self","max","self","self","self","self","self","self","max","max"]' >> vcfanno.conf
 printf "\n" >> vcfanno.conf
 echo '[[annotation]]' >> vcfanno.conf
 echo 'file="'$DIR'/civic/civic_variants_summaries.bed.gz"' >> vcfanno.conf
-echo 'columns=[6,6,8,9,12,13,14,15,16,11,17,18]' >> vcfanno.conf
+echo 'columns=[6,6,8,9,12,13,14,15,16,11,17,18,19]' >> vcfanno.conf
 echo 'names=["in_civic","civic_var_id","civic_score","civic_percentile","civic_evi_type","civic_evi_level","civic_evi_direction","civic_clin_sig","civic_rating","civic_evi_id","civic_var_origin","civic_disease","civic_abbreviations"]' >> vcfanno.conf
 echo 'ops=["flag","concat","concat","concat","concat","concat","concat","concat","concat","concat", "concat", "concat","concat"]' >> vcfanno.conf
 
